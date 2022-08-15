af6ac03c5cbaa8cb9940418e665eb3e99cb11496
Leximon <lex305@web.de>
2022-08-15 23:40:47 +0200

    fix being able to place blocks everywhere without condition

--- src/main/java/de/leximon/fluidlogged/mixin/BlockItemMixin.java
+++ src/main/java/de/leximon/fluidlogged/mixin/BlockItemMixin.java
@@ -17,22 +17,17 @@ public abstract class BlockItemMixin {
 
     @Inject(method = "getPlacementState", at = @At("RETURN"), cancellable = true)
     private void injectCustomFluidPlacementState(ItemPlacementContext ctx, CallbackInfoReturnable<BlockState> cir) {
-        BlockState placementState = this.getBlock().getPlacementState(ctx);
-        if (placementState == null) {
-            cir.setReturnValue(null);
+        BlockState placementState = cir.getReturnValue();
+        if (placementState == null)
             return;
-        }
         if (!placementState.contains(FluidloggedMod.PROPERTY_FLUID)) {
             cir.setReturnValue(placementState);
             return;
         }
         FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
         int index = FluidloggedMod.getFluidIndex(fluidState.getFluid());
-        if(index != -1) {
+        if(index != -1)
             cir.setReturnValue(placementState.with(FluidloggedMod.PROPERTY_FLUID, index));
-            return;
-        }
-        cir.setReturnValue(placementState);
     }
 
     @Shadow public abstract Block getBlock();
