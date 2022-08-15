481911167555222d7e916854272cd84486f79cdc
Leximon <lex305@web.de>
2022-08-16 00:19:58 +0200

    optimization

--- src/main/java/de/leximon/fluidlogged/mixin/BlockItemMixin.java
+++ src/main/java/de/leximon/fluidlogged/mixin/BlockItemMixin.java
@@ -10,24 +10,38 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BlockItem.class)
 public abstract class BlockItemMixin {
+//
+//    @Inject(method = "getPlacementState", at = @At("RETURN"), cancellable = true)
+//    private void injectCustomFluidPlacementState(ItemPlacementContext ctx, CallbackInfoReturnable<BlockState> cir) {
+//        BlockState placementState = cir.getReturnValue();
+//        if (placementState == null)
+//            return;
+//        if (!placementState.contains(FluidloggedMod.PROPERTY_FLUID)) {
+//            cir.setReturnValue(placementState);
+//            return;
+//        }
+//        FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
+//        int index = FluidloggedMod.getFluidIndex(fluidState.getFluid());
+//        if(index != -1)
+//            cir.setReturnValue(placementState.with(FluidloggedMod.PROPERTY_FLUID, index));
+//    }
 
-    @Inject(method = "getPlacementState", at = @At("RETURN"), cancellable = true)
-    private void injectCustomFluidPlacementState(ItemPlacementContext ctx, CallbackInfoReturnable<BlockState> cir) {
-        BlockState placementState = cir.getReturnValue();
+    @ModifyVariable(method = "getPlacementState", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/BlockItem;canPlace(Lnet/minecraft/item/ItemPlacementContext;Lnet/minecraft/block/BlockState;)Z"), ordinal = 0)
+    private BlockState injectCustomFluidPlacementState(BlockState placementState, ItemPlacementContext ctx) {
         if (placementState == null)
-            return;
-        if (!placementState.contains(FluidloggedMod.PROPERTY_FLUID)) {
-            cir.setReturnValue(placementState);
-            return;
-        }
+            return null;
+        if (!placementState.contains(FluidloggedMod.PROPERTY_FLUID))
+            return placementState;
         FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
         int index = FluidloggedMod.getFluidIndex(fluidState.getFluid());
-        if(index != -1)
-            cir.setReturnValue(placementState.with(FluidloggedMod.PROPERTY_FLUID, index));
+        if (index != -1)
+            return placementState.with(FluidloggedMod.PROPERTY_FLUID, index);
+        return placementState;
     }
 
     @Shadow public abstract Block getBlock();
